3bf12323c18bed77032a4866b11e9eff000ee203
Sheelah Brennan <slb53@hotmail.com>
2015-07-09 15:23:10 -0600

    Update TGM plugin for bundled plugin needs

--- lib/theme-require-plugins.php
+++ lib/theme-require-plugins.php
@@ -3,7 +3,7 @@
 /**
  * Register the required plugins for this theme.
  */
-function sandia_register_required_plugins() {
+function sheelahb_register_required_plugins() {
 
 	$plugins = array(
 
@@ -11,17 +11,22 @@ function sandia_register_required_plugins() {
 			'name' 				=> 'WordPress SEO by Yoast',
 			'slug' 				=> 'wordpress-seo',
 			'required' 			=> true,
-			'force_activation'	=> true
+			'force_activation'	=> true,
+			'is_callable'       => 'wpseo_init',
 		),
 
-		array(
-			'name' 				=> 'Google Analytics for WordPress',
-			'slug' 				=> 'google-analytics-for-wordpress',
-			'required' 			=> true,
-			'force_activation'	=> true
-		),
+//		array(
+//			'name' 				=> 'Google Analytics for WordPress',
+//			'slug' 				=> 'google-analytics-for-wordpress',
+//			'required' 			=> true,
+//			'force_activation'	=> true
+//		),
 
 	);
+
+	// Change this to your theme text domain, used for internationalising strings
+	$theme_text_domain = 'sandia';
+
 
 	/**
 	 * Array of configuration settings. Amend each line as needed.
@@ -31,36 +36,34 @@ function sandia_register_required_plugins() {
 	 * end of each line for what each argument will be.
 	 */
 	$config = array(
-		'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
-		'default_path' => '',                      // Default absolute path to pre-packaged plugins.
-		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'has_notices'  => true,                    // Show admin notices or not.
-		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-		'message'      => '',                      // Message to output right before the plugins table.
-		'strings'      => array(
-			'page_title'                      => __( 'Install Required Plugins', 'tgmpa', 'sandia' ),
-			'menu_title'                      => __( 'Install Plugins', 'tgmpa', 'sandia' ),
-			'installing'                      => __( 'Installing Plugin: %s', 'tgmpa', 'sandia' ), // %s = plugin name.
-			'oops'                            => __( 'Something went wrong with the plugin API.', 'tgmpa', 'sandia' ),
-			'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.', 'tgmpa', 'sandia' ), // %1$s = plugin name(s).
-			'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.', 'tgmpa', 'sandia' ), // %1$s = plugin name(s).
-			'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.', 'tgmpa', 'sandia' ), // %1$s = plugin name(s).
-			'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.', 'tgmpa', 'sandia' ), // %1$s = plugin name(s).
-			'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.', 'tgmpa', 'sandia' ), // %1$s = plugin name(s).
-			'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.', 'tgmpa', 'sandia' ), // %1$s = plugin name(s).
-			'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.', 'tgmpa', 'sandia' ), // %1$s = plugin name(s).
-			'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.', 'tgmpa', 'sandia' ), // %1$s = plugin name(s).
-			'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins', 'tgmpa', 'sandia' ),
-			'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins', 'tgmpa', 'sandia' ),
-			'return'                          => __( 'Return to Required Plugins Installer', 'tgmpa', 'sandia' ),
-			'plugin_activated'                => __( 'Plugin activated successfully.', 'tgmpa', 'sandia' ),
-			'complete'                        => __( 'All plugins installed and activated successfully. %s', 'tgmpa', 'sandia' ), // %s = dashboard link.
-			'nag_type'                        => 'updated' // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
+		'domain'       		=> $theme_text_domain,         	// Text domain - likely want to be the same as your theme.
+		'default_path' 		=> '',                         	// Default absolute path to pre-packaged plugins
+		'parent_slug' 	=> 'themes.php', 				// Default parent menu slug
+		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'menu'         		=> 'install-required-plugins', 	// Menu slug
+		'has_notices'      	=> true,                       	// Show admin notices or not
+		'is_automatic'    	=> false,					   	// Automatically activate plugins after installation or not
+		'message' 			=> '',							// Message to output right before the plugins table
+		'strings'      		=> array(
+			'page_title'                       			=> __( 'Install Required Plugins', $theme_text_domain ),
+			'menu_title'                       			=> __( 'Install Plugins', $theme_text_domain ),
+			'installing'                       			=> __( 'Installing Plugin: %s', $theme_text_domain ), // %1$s = plugin name
+			'oops'                             			=> __( 'Something went wrong with the plugin API.', $theme_text_domain ),
+			'notice_can_install_required'     			=> _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ), // %1$s = plugin name(s)
+			'notice_can_install_recommended'			=> _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ), // %1$s = plugin name(s)
+			'notice_cannot_install'  					=> _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s)
+			'notice_can_activate_required'    			=> _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s)
+			'notice_can_activate_recommended'			=> _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s)
+			'notice_cannot_activate' 					=> _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ), // %1$s = plugin name(s)
+			'notice_ask_to_update' 						=> _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ), // %1$s = plugin name(s)
+			'notice_cannot_update' 						=> _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s)
+			'install_link' 					  			=> _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
+			'activate_link' 				  			=> _n_noop( 'Activate installed plugin', 'Activate installed plugins' ),
+			'return'                           			=> __( 'Return to Required Plugins Installer', $theme_text_domain ),
+			'plugin_activated'                 			=> __( 'Plugin activated successfully.', $theme_text_domain ),
+			'complete' 									=> __( 'All plugins installed and activated successfully. %s', $theme_text_domain ), // %1$s = dashboard link
+			'nag_type'									=> 'updated' // Determines admin notice type - can only be 'updated' or 'error'
 		)
 	);
-
 	tgmpa( $plugins, $config );
-
 }
